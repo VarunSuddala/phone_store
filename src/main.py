0da4b5dd817226a1a262057eecffdbebdb6cebfd
VarunSuddala <2403a53l02@sru.edu.in>
2026-01-16 00:29:20 +0530

    removed all junk modules

--- src/main.py
+++ src/main.py
@@ -1,7 +1,4 @@
-import fastapi
-from fastapi import FastAPI, HTTPException,Query
-from pydantic import BaseModel
-import app
+from src.app import app
 import uvicorn
 if __name__=="__main__":
-    uvicorn.run(app.app,host="localhost",port=8000,reload=True)
+    uvicorn.run(app,host="localhost",port=8000)
